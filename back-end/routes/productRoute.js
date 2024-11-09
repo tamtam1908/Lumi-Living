4affd2379387e169d1cc96783e424a607923b7a9
Tam Hoang <119677178+tamtam1908@users.noreply.github.com>
2024-11-09 20:38:59 +0700

    code ket noi voi atlas mongo va tao user

---
+++ back-end/routes/productRoute.js
@@ -0,0 +1,12 @@
+import {addProduct, removeProduct, listProduct, singleProduct} from '../controllers/productController.js';
+import express from 'express';
+import upload from '../middleware/multer.js';
+
+const productRouter = express.Router();
+
+productRouter.post('/add',upload.fields([{name: 'image1', maxCount:1},{name: 'image2', maxCount:1},{name: 'image3', maxCount:1},{name: 'image4', maxCount:1}]), addProduct);
+productRouter.post('/remove', removeProduct);
+productRouter.post('/single', singleProduct);
+productRouter.post('/list', listProduct);
+
+export default productRouter
