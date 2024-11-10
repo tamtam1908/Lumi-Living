b0cf0a5bb90d2cda32f9a696d49b9f4c72d3679b
nguyenlekhaai <164020914+nguyenlekhaai@users.noreply.github.com>
2024-11-11 00:16:37 +0700

    admin prolist & add backend to frontend

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import { assets } from '../assets/assets'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCirclePlus, faList, faBasketShopping} from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
   return (
     <div className='w-[100%] min-h-screen border-r-2'>
       <div className='flex flex-col gap-4 pt-6 pl-[10%] text-[15px]'>
         <NavLink className='navlink' to="/add">
-            <img src={assets.add_icon} alt="" />
+            <FontAwesomeIcon icon={faCirclePlus} />
             <p className='hidden md:block'>Thêm sản phẩm</p>
         </NavLink>
         <NavLink className='navlink' to="/list">
-            <img src={assets.order_icon} alt="" />
+            <FontAwesomeIcon icon={faList} />
             <p className='hidden md:block'>Danh sách sản phẩm</p>
         </NavLink>
         <NavLink className='navlink' to="/orders">
-            <img src={assets.order_icon} alt="" />
+            <FontAwesomeIcon icon={faBasketShopping} />
             <p className='hidden md:block'>Đơn hàng</p>
         </NavLink>
 
--- front-end/src/context/ShopContext.jsx
+++ front-end/src/context/ShopContext.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import { products } from '../assets/assets';
+import React, { useEffect, useState } from 'react';
+// import { products } from '../assets/assets';
 import { createContext } from 'react';
-
+import axios from 'axios'
 
 export const ShopContext = createContext()
 
 const ShopContextProvider = (props) => {
   const currency = 'VND' ;
   const delivery_fee = 10;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL
+  const [products, setProducts] = useState([]);
+  
+  const getProductsData = async () => {
+    try {
+      
+      const response = await axios.get(backendUrl + '/api/product/list') 
+      if (response.data.success){
+        setProducts(response.data.products);
+      }
+      else{
+        toast.error(response.data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(()=>{
+    getProductsData()
+  })
+
   const value = {
-    products, currency, delivery_fee
+    products, currency, delivery_fee, backendUrl
   } 
+  
   return (
     <ShopContext.Provider value={value}>
         {props.children}
